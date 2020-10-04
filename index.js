f97c9f730a38e08a8611ee4f85d6ad56547df71c
Alessandro <alessandroadm@live.com>
2020-10-04 16:17:02 -0300

    refactor: rename function and update filename

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-const CriarNovoArquivo = require('./src/modules/createFile');
-const VerificarSeArquivoExiste = require('./src/modules/checkFileExist');
-const EscreverArquivo = require('./src/modules/writeFile');
-const LerArquivo = require('./src/modules/readFile');
+const CriarArquivo = require('./src/modules/createFile');
+const ArquivoExiste = require('./src/modules/checkFileExist');
+const SobrescreverDadosNoArquivo = require('./src/modules/writeFile');
+const LerDadosDeUmArquivo = require('./src/modules/readFile');
 const AdicionarValoresEmArquivo = require('./src/modules/appendDataFile');
-const ExcluirUmArquivo = require('./src/modules/deleteFile');
+const ExcluirArquivo = require('./src/modules/deleteFile');
 
-
-const CriarArquivo = async (nomeArquivo) => await CriarNovoArquivo(nomeArquivo);
-const ArquivoExiste = async (nomeArquivo) => await VerificarSeArquivoExiste(nomeArquivo);
-const EscreverEmUmArquivo = async (nomeArquivo, dados) => await EscreverArquivo(nomeArquivo, dados);
-const LerDadosDeUmArquivo = async (nomeArquivo) => await LerArquivo(nomeArquivo);
-const AdicionarValores = async (nomeArquivo, dados) => await AdicionarValoresEmArquivo(nomeArquivo, dados);
-const ExcluirArquivo = async (nomeArquivo) => await ExcluirUmArquivo(nomeArquivo);
 
 
 const CriarUmArquivoEscreverHelloWorldNeleImprimirEExcluir = async (nomeDoArquivo) => {
- if (await ArquivoExiste(nomeDoArquivo)) {
-  await AdicionarValores(nomeDoArquivo, 'Hello World');
- } else {
+ if (!(await ArquivoExiste(nomeDoArquivo))) {
   await CriarArquivo(nomeDoArquivo);
- }
- await EscreverEmUmArquivo(nomeDoArquivo, 'Hello World');
+ } 
+ await AdicionarValoresEmArquivo(nomeDoArquivo, 'Hello World');
  console.table(await LerDadosDeUmArquivo(nomeDoArquivo));
+ await SobrescreverDadosNoArquivo(nomeDoArquivo, 'Deus é fiel');
+ console.log(await LerDadosDeUmArquivo(nomeDoArquivo));
  await ExcluirArquivo(nomeDoArquivo);
 }
 
