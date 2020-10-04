016b37cc357329c8945896d6268c1511743bb358
Alessandro dev <34426407+ALESSANDROLMENEZES@users.noreply.github.com>
2020-10-04 15:08:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-import { readFile } from './src/modules/read.js'
-import { writeFile } from './src/modules/write.js'
-
-const read = async () => {
- console.log(await readFile('w.json'));
-}
-
-const write = async () => {
- console.log(await writeFile('index.json', 'Prisma'));
-}
-
-read();
-//write();
+//Metodos implementados na pasta src/modules
