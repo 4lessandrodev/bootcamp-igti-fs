6e1e6ff92ae70bb5c9bb1e05443f68b514f2ceda
Alessandro <alessandroadm@live.com>
2020-10-04 16:07:48 -0300

    feat: create index to create a file and type on it

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-//Metodos implementados na pasta src/modules
+const CriarNovoArquivo = require('./src/modules/createFile');
+const VerificarSeArquivoExiste = require('./src/modules/checkFileExist');
+const EscreverArquivo = require('./src/modules/writeFile');
+const LerArquivo = require('./src/modules/readFile');
+const AdicionarValoresEmArquivo = require('./src/modules/appendDataFile');
+const ExcluirUmArquivo = require('./src/modules/deleteFile');
+
+
+const CriarArquivo = async (nomeArquivo) => await CriarNovoArquivo(nomeArquivo);
+const ArquivoExiste = async (nomeArquivo) => await VerificarSeArquivoExiste(nomeArquivo);
+const EscreverEmUmArquivo = async (nomeArquivo, dados) => await EscreverArquivo(nomeArquivo, dados);
+const LerDadosDeUmArquivo = async (nomeArquivo) => await LerArquivo(nomeArquivo);
+const AdicionarValores = async (nomeArquivo, dados) => await AdicionarValoresEmArquivo(nomeArquivo, dados);
+const ExcluirArquivo = async (nomeArquivo) => await ExcluirUmArquivo(nomeArquivo);
+
+
+const CriarUmArquivoEscreverHelloWorldNeleImprimirEExcluir = async (nomeDoArquivo) => {
+ if (await ArquivoExiste(nomeDoArquivo)) {
+  await AdicionarValores(nomeDoArquivo, 'Hello World');
+ } else {
+  await CriarArquivo(nomeDoArquivo);
+ }
+ await EscreverEmUmArquivo(nomeDoArquivo, 'Hello World');
+ console.table(await LerDadosDeUmArquivo(nomeDoArquivo));
+ await ExcluirArquivo(nomeDoArquivo);
+}
+
+CriarUmArquivoEscreverHelloWorldNeleImprimirEExcluir('teste.json');
+  
+
--- src/modules/createFile.js
+++ src/modules/createFile.js
@@ -4,7 +4,7 @@ const path = require('path');
 const FILE_PATH = require('./variables.js');
 
 const createFile = async (filename) => {
- await fs.writeFile(path.join(FILE_PATH, filename), JSON.stringify({data:[]}));
+ return await fs.writeFile(path.join(FILE_PATH, filename), JSON.stringify({data:[]}));
 }
 
 module.exports = createFile;
